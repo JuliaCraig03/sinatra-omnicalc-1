f85cefc05aac9a3e2cb2ae790e92dfa82f74ed35
JuliaCraig03 <juliacraig@uchicago.edu>
2024-10-21 19:58:33 +0000

    3/4 Way There

--- app.rb
+++ app.rb
@@ -36,3 +36,17 @@ get("/square_root/results") do
 
   erb(:square_root_results)
 end
+
+get("/random/new") do
+  erb(:random)
+end
+
+get("/random/results") do
+  @min_num = params.fetch("user_min").to_f
+  @max_num = params.fetch("user_max").to_f
+
+  # Ensure @min_num is smaller than @max_num
+  @the_result = rand(@min_num..@max_num)
+
+  erb(:random_results)
+end
