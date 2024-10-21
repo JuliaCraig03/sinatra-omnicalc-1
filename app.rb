a7dbdebedbbdd9cd1b3dac4b5eae65848d810949
JuliaCraig03 <juliacraig@uchicago.edu>
2024-10-21 17:19:22 +0000

    Halfway Point

--- app.rb
+++ app.rb
@@ -10,68 +10,29 @@ erb(:bye)
 end
 
 get("/square/new") do
-erb(:new_square_calc)
+  erb(:new_square_calc)
 end
 
 get("/square/results") do
- @the_num = params.fetch("users_number").to_f
+  @the_num = params.fetch("users_number").to_f
 
   @the_result = @the_num ** 2
 
-    erb(:square_results)
+  erb(:square_results)
 end
 
 get("/") do
-  require "sinatra"
-require "sinatra/reloader"
-
-get("/howdy")do
-erb(:hello)
+  erb(:new_square_calc)
 end
 
-get("/goodbye")do
-erb(:bye)
+get("/square_root/new") do
+  erb(:square_root_new)
 end
 
-get("/square/new") do
-erb(:new_square_calc)
-end
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
 
-get("/square/results") do
- @the_num = params.fetch("users_number").to_f
+  @the_result = @the_num ** 0.5
 
-  @the_result = @the_num ** 2
-
-    erb(:square_results)
-end
-
-get("/") do
-  require "sinatra"
-require "sinatra/reloader"
-
-get("/howdy")do
-erb(:hello)
-end
-
-get("/goodbye")do
-erb(:bye)
-end
-
-get("/square/new") do
-erb(:new_square_calc)
-end
-
-get("/square/results") do
- @the_num = params.fetch("users_number").to_f
-
-  @the_result = @the_num ** 2
-
-    erb(:square_results)
-end
-
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square_root_results)
 end
